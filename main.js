01c0f82d642600241dbb32ac9a962af3c20277bc
Dallss <randallalquicer.school@gmail.com>
2024-05-28 00:29:43 +0800

    used polar coordinates for animating. Use same logic on other corners. adjust for side pieces. twas fun :)

--- main.js
+++ main.js
@@ -4,15 +4,21 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 import * as THREE from 'three';
-import { cameraPosition } from 'three/examples/jsm/nodes/Nodes.js';
+import { cameraPosition, instance, sqrt } from 'three/examples/jsm/nodes/Nodes.js';
 
 //manual imports
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { degToRad, radToDeg } from 'three/src/math/MathUtils.js';
+
+
+//helper functions
+function degreesToRadians(degrees) {
+  return degrees * (Math.PI / 180);
+}
 
 
 //preps
 const scene = new THREE.Scene();
-
 const cam = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, .01, 1000); //(sixe of angle perspective, aspect ratio, view frustum n, viem frustum m )
 
 const renderer = new THREE.WebGLRenderer(
@@ -22,146 +28,124 @@ const renderer = new THREE.WebGLRenderer(
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth*0.6, window.innerHeight*0.6);
 
-
 //camera
 cam.position.setZ(30);
 const orbitcont = new OrbitControls(cam, renderer.domElement);
 
-
-
-
 //gemoetries
 const cubeGeometry = new THREE.BoxGeometry(10, 10, 10);
-const cubePiece = new THREE.BoxGeometry(5,5,5);
-
-
+const cubePiece = new THREE.BoxGeometry(4.5,4.5,4.5);
 
 //materials
 const materialgray = new THREE.MeshBasicMaterial({color: 0x808080 ,wireframe: true });
-const materialblue = new THREE.MeshBasicMaterial({color: 0x0000ff, wireframe: true});
 
-// Define materials for each face
-const materialRed = new THREE.MeshBasicMaterial({ color: 0xff0000 }); // Red
-const materialGreen = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); // Green
-const materialBlue = new THREE.MeshBasicMaterial({ color: 0x0000ff }); // Blue
-const materialYellow = new THREE.MeshBasicMaterial({ color: 0xffff00 }); // Yellow
-const materialWhite = new THREE.MeshBasicMaterial({ color: 0xffffff }); // White
-const materialOrange = new THREE.MeshBasicMaterial({ color: 0xffa500 }); // Orange
+const color = [
+  new THREE.MeshBasicMaterial({ color: 0xFFFFFF }), // white     +x
+  new THREE.MeshBasicMaterial({ color: 0xFFD500 }), // yellow    -x
+  new THREE.MeshBasicMaterial({ color: 0xB90000 }), // red       +y
+  new THREE.MeshBasicMaterial({ color: 0xFF5900 }), // Orange    -y
+  new THREE.MeshBasicMaterial({ color: 0x0045AD }), // blue      +z
+  new THREE.MeshBasicMaterial({ color: 0x009B48 })  // green     -z
+];
 
 //objects
 const axesHelper = new THREE.AxesHelper(25);
 const cubeMesh = new THREE.Mesh(cubeGeometry, materialgray);
 
-const c000 = new THREE.Mesh(cubePiece, materialblue);
-const c001 = new THREE.Mesh(cubePiece, materialblue);
-const c002 = new THREE.Mesh(cubePiece, materialblue);
-const c010 = new THREE.Mesh(cubePiece, materialblue);
-const c011 = new THREE.Mesh(cubePiece, materialblue);
-const c012 = new THREE.Mesh(cubePiece, materialblue);
-const c020 = new THREE.Mesh(cubePiece, materialblue);
-const c021 = new THREE.Mesh(cubePiece, materialblue);
-const c022 = new THREE.Mesh(cubePiece, materialblue);
 
-const c100 = new THREE.Mesh(cubePiece, materialblue);
-const c101 = new THREE.Mesh(cubePiece, materialblue);
-const c102 = new THREE.Mesh(cubePiece, materialblue);
-const c110 = new THREE.Mesh(cubePiece, materialblue);
-const c111 = new THREE.Mesh(cubePiece, materialblue);
-const c112 = new THREE.Mesh(cubePiece, materialblue);
-const c120 = new THREE.Mesh(cubePiece, materialblue);
-const c121 = new THREE.Mesh(cubePiece, materialblue);
-const c122 = new THREE.Mesh(cubePiece, materialblue);
+class Cube {
+  constructor(scene) {
+    this.piece = [];
 
-const c200 = new THREE.Mesh(cubePiece, materialblue);
-const c201 = new THREE.Mesh(cubePiece, materialblue);
-const c202 = new THREE.Mesh(cubePiece, materialblue);
-const c210 = new THREE.Mesh(cubePiece, materialblue);
-const c211 = new THREE.Mesh(cubePiece, materialblue);
-const c212 = new THREE.Mesh(cubePiece, materialblue);
-const c220 = new THREE.Mesh(cubePiece, materialblue);
-const c221 = new THREE.Mesh(cubePiece, materialblue);
-const c222 = new THREE.Mesh(cubePiece, materialblue);
+    // let (0,0,0) be corner on all negatives, and ijk correspond to xyz
+    for (let i = 0, x = -5; i < 3; i++, x += 5) {
+      this.piece[i] = [];
+      for (let j = 0, y = -5; j < 3; j++, y += 5) {
+        this.piece[i][j] = [];
+        for (let k = 0, z = -5; k < 3; k++, z += 5) {
+          this.piece[i][j][k] = new THREE.Mesh(cubePiece, color);
+          this.piece[i][j][k].position.set(x,y,z);
+          scene.add(this.piece[i][j][k]);
+        }
+      }
+    }
+  }
+}
+
+//turn top functions
 
 
-//set postions
-c000.position.set(5,5,5);
-c001.position.set(5,5,0);
-c002.position.set(5,5,-5);
-c010.position.set(0,5,5);
-c011.position.set(0,5,0);
-c012.position.set(0,5,-5);
-c020.position.set(-5,5,5);
-c021.position.set(-5,5,0);
-c022.position.set(-5,5,-5);
+//keep track of angle
 
-c100.position.set(5,0,5);
-c101.position.set(5,0,0);
-c102.position.set(5,0,-5);
-c110.position.set(0,0,5);
-c111.position.set(0,0,0);
-c112.position.set(0,0,-5);
-c120.position.set(-5,0,5);
-c121.position.set(-5,0,0);
-c122.position.set(-5,0,-5);
+let angle = 45
 
-c200.position.set(5,-5,5);
-c201.position.set(5,-5,0);
-c202.position.set(5,-5,-5);
-c210.position.set(0,-5,5);
-c211.position.set(0,-5,0);
-c212.position.set(0,-5,-5);
-c220.position.set(-5,-5,5);
-c221.position.set(-5,-5,0);
-c222.position.set(-5,-5,-5);
+function turn(clockwise, cube){
 
-//adding to scenes
+  let initx = cube.piece[2][2][2].position.x
+  let initz = cube.piece[2][2][2].position.z
+
+  const radius = Math.sqrt(Math.pow(initx,2)+Math.pow(initz,2));
+
+
+  //corners
+  
+  cube.piece[2][2][2].position.x = radius * Math.sin(degToRad(angle));
+  cube.piece[2][2][2].position.z = radius * Math.cos(degToRad(angle));
+  cube.piece[2][2][2].rotation.y = degToRad(angle-45);
+  // cube.piece[2][2][0].position.x = radius * Math.sin(angle);
+  // cube.piece[2][2][0].position.z = radius * Math.cos(angle);
+
+  // cube.piece[0][2][2].position.x = radius * Math.sin(angle);
+  // cube.piece[0][2][2].position.z = radius * Math.cos(angle);
+
+  // cube.piece[0][2][0].position.x = radius * Math.sin(angle);
+  // cube.piece[0][2][0].position.z = radius * Math.cos(angle);
+
+
+  // cube.piece[2][2][0].rotation.y = angle - 44.8;
+  // cube.piece[0][2][0].rotation.y = angle - 44.8;
+
+  angle += 1 ;
+
+}
+let turning = false;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const rotateButton = document.querySelector(".rotate");
+  rotateButton.addEventListener("click", onpress);
+
+  function onpress() {
+    if(!turning){
+      console.log("Button clicked!");
+      turning = true;
+    }
+  }
+});
+
+
 scene.add(axesHelper);
-scene.add(cubeMesh);
-
-scene.add(c000);
-scene.add(c001);
-scene.add(c002);
-scene.add(c010);
-scene.add(c011);
-scene.add(c012);
-scene.add(c020);
-scene.add(c021);
-scene.add(c022);
-
-scene.add(c100);
-scene.add(c101);
-scene.add(c102);
-scene.add(c110);
-scene.add(c111);
-scene.add(c112);
-scene.add(c120);
-scene.add(c121);
-scene.add(c122);
-
-scene.add(c200);
-scene.add(c201);
-scene.add(c202);
-scene.add(c210);
-scene.add(c211);
-scene.add(c212);
-scene.add(c220);
-scene.add(c221);
-scene.add(c222);
-
-renderer.render(scene, cam);
-
-//set colors
 
 
-
-//gameloop
+let rubikz = new Cube(scene);
+let speed = 0;
 function animate (){
   requestAnimationFrame( animate );
-
   
+  if(turning){
+    turn(true,rubikz);
+    if((angle-45) % 90 == 0){
+      turn(true, rubikz)
+      turning = false
+    }
+  }
+  
+
+  // console.log(rubikz.piece[2][2][2].position.x)
+  // console.log(rubikz.piece[2][2][2].position.z)
+  console.log(angle)
+
   renderer.render(scene, cam);
-
-  
 }
 
 animate();
+
