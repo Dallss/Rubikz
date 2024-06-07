5b203347b36d8dc6b35adecd908861982e4f46fe
jedlaptop <randallalquicer21@gmail.com>
2024-06-07 10:21:33 +0700

    First Commit

--- main.js
+++ main.js
@@ -55,27 +55,7 @@ const color = [
 const axesHelper = new THREE.AxesHelper(25)
 scene.add(axesHelper)
 
-
-function init(){
-  let radiuscorner = Math.sqrt(5**2 + 5**2)
-  
-  window.piece = [];  // Declare piece as a global array
-  for (let i = 0, x = -5; i < 3; i++, x += 5) {
-    piece[i] = []
-    for (let j = 0, y = -5; j < 3; j++, y += 5) {
-      piece[i][j] = []
-      for (let k = 0, z = -5; k < 3; k++, z += 5) {
-        piece[i][j][k] = new THREE.Mesh(cubePiece, color)
-        piece[i][j][k].position.set(x,y,z)
-
-        scene.add(piece[i][j][k])
-      }
-    }
-  }
-  
- //
-}
-
+//utility functions
 function getAngleOnXZPlane(piece){
   let x = piece.position.x
   let z = piece.position.z
@@ -91,8 +71,6 @@ function getAngleOnZYPlane(piece){
   let y = piece.position.y
   return Math.atan2(y,z)
 }
-
-
 
 function rotateOnYAxis(piece,degrees){
   
@@ -133,243 +111,311 @@ function rotateOnZAxis(piece,degrees){
 
 
 
-function turnY(degrees, layer){ //ylayer 2= top, 0=bottom
-
-  for(let i=0; i<3; i++){
-    for(let j=0; j<3; j++){
-      if(i == 1 && j == 1)
-        continue
-      rotateOnYAxis(piece[i][layer][j],degrees)
-    }
-  }
-  //mid
-  const axis = new THREE.Vector3(0,1,0)
-  piece[1][layer][1].rotateOnWorldAxis(axis,degToRad(-degrees))
-}
-function turnX(degrees, layer){
-  for(let i=0; i<3; i++){
-    for(let j=0; j<3; j++){
-      if(i == 1 && j == 1)
-        continue
-      rotateOnXAxis(piece[layer][i][j],degrees)
-    }
-  }
-  //mid
-  const axis = new THREE.Vector3(1,0,0)
-  piece[layer][1][1].rotateOnWorldAxis(axis,degToRad(-degrees))
-
-}
-function turnZ(degrees, layer){
-  for(let i=0; i<3; i++){
-    for(let j=0; j<3; j++){
-      if(i == 1 && j == 1)
-        continue
-      rotateOnZAxis(piece[j][i][layer],degrees)
-    }
-  }
-  //mid
-  const axis = new THREE.Vector3(0,0,1)
-  piece[1][1][layer].rotateOnWorldAxis(axis,degToRad(-degrees))
-}
 
 
 
-function setZArray(direction, layer){
+
+
+
+class Cube{
+  constructor(scene){
+    let radiuscorner = Math.sqrt(5**2 + 5**2)
   
-  if(direction > 0){
-    let hold = piece[0][2][layer];  
-    // Rotating the corners clockwise
-    piece[0][2][layer] = piece[0][0][layer];
-    piece[0][0][layer] = piece[2][0][layer];
-    piece[2][0][layer] = piece[2][2][layer];
-    piece[2][2][layer] = hold;
+    //I needed something to identify each and indivual piece to know the cube's state [solved, not]
+    // So I used its solved state coordinate [i][j][k] and made a separate 3d array for the frame that the move functions
 
-    // Save the initial edge piece
-    hold = piece[0][1][layer];
+    this.piece = [];  // Declare this.piece as a global array
+    this.cubeFrame = [];
 
-    // Rotating the edges clockwise
-    piece[0][1][layer] = piece[1][0][layer];
-    piece[1][0][layer] = piece[2][1][layer];
-    piece[2][1][layer] = piece[1][2][layer];
-    piece[1][2][layer] = hold;
+    for (let i = 0, x = -5; i < 3; i++, x += 5) {
+      this.piece[i] = []
+      this.cubeFrame [i] = []
+
+      for (let j = 0, y = -5; j < 3; j++, y += 5) {
+
+        this.piece[i][j] = []
+        this.cubeFrame[i][j] = []
+        for (let k = 0, z = -5; k < 3; k++, z += 5) {
+
+          this.piece[i][j][k] = new THREE.Mesh(cubePiece, color)
+          this.piece[i][j][k].position.set(x,y,z)
+          scene.add(this.piece[i][j][k])
+
+          this.cubeFrame[i][j][k] = this.piece[i][j][k]
+        }
+      }
+    }
   }
-  else {
-    let hold = piece[0][2][layer];
-    // Rotating the corners counterclockwise
-    piece[0][2][layer] = piece[2][2][layer];
-    piece[2][2][layer] = piece[2][0][layer];
-    piece[2][0][layer] = piece[0][0][layer];
-    piece[0][0][layer] = hold;
 
-    // Save the initial edge piece
-    hold = piece[0][1][layer];
+  turnY(degrees, layer){ //ylayer 2= top, 0=bottom
 
-    // Rotating the edges counterclockwise
-    piece[0][1][layer] = piece[1][2][layer];
-    piece[1][2][layer] = piece[2][1][layer];
-    piece[2][1][layer] = piece[1][0][layer];
-    piece[1][0][layer] = hold;
+    for(let i=0; i<3; i++){
+      for(let j=0; j<3; j++){
+        if(i == 1 && j == 1)
+          continue
+        rotateOnYAxis(this.cubeFrame[i][layer][j],degrees)
+      }
+    }
+    //mid
+    const axis = new THREE.Vector3(0,1,0)
+    this.cubeFrame[1][layer][1].rotateOnWorldAxis(axis,degToRad(-degrees))
   }
-  //for counter clockwise
-}
-function setXArray(direction, layer){
-
-  if (direction > 0) {
-    //Rotating the corners clockwise
-    let hold = piece[layer][0][0];  
-    piece[layer][0][0] = piece[layer][2][0];
-    piece[layer][2][0] = piece[layer][2][2];
-    piece[layer][2][2] = piece[layer][0][2];
-    piece[layer][0][2] = hold;
-
-    // Save the initial edge piece
-    hold = piece[layer][0][1];
-
-    // Rotating the edges clockwise
-    piece[layer][0][1] = piece[layer][1][0];
-    piece[layer][1][0] = piece[layer][2][1];
-    piece[layer][2][1] = piece[layer][1][2];
-    piece[layer][1][2] = hold;
-
+  turnX(degrees, layer){
+    for(let i=0; i<3; i++){
+      for(let j=0; j<3; j++){
+        if(i == 1 && j == 1)
+          continue
+        rotateOnXAxis(this.cubeFrame[layer][i][j],degrees)
+      }
+    }
+    //mid
+    const axis = new THREE.Vector3(1,0,0)
+    this.cubeFrame[layer][1][1].rotateOnWorldAxis(axis,degToRad(-degrees))
+  
   }
-  else {
-    // Rotating the corners counterclockwise
-    let hold = piece[layer][0][0];
-    piece[layer][0][0] = piece[layer][0][2];
-    piece[layer][0][2] = piece[layer][2][2];
-    piece[layer][2][2] = piece[layer][2][0];
-    piece[layer][2][0] = hold;
-
-    // Save the initial edge piece
-    hold = piece[layer][0][1];
-
-    // Rotating the edges counterclockwise
-    piece[layer][0][1] = piece[layer][1][2];
-    piece[layer][1][2] = piece[layer][2][1];
-    piece[layer][2][1] = piece[layer][1][0];
-    piece[layer][1][0] = hold;
-}
-}
-function setYArray(direction, layer){
- 
-  if (direction > 0) {
-    // Rotating the corners clockwise
-    let hold = piece[2][layer][2];  
-
-    piece[2][layer][2] = piece[2][layer][0];
-    piece[2][layer][0] = piece[0][layer][0];
-    piece[0][layer][0] = piece[0][layer][2];
-    piece[0][layer][2] = hold;
-
-    // Save the initial edge piece
-    hold = piece[1][layer][0];
-
-    // Rotating the edges clockwise
-    piece[1][layer][0] = piece[0][layer][1];
-    piece[0][layer][1] = piece[1][layer][2];
-    piece[1][layer][2] = piece[2][layer][1];
-    piece[2][layer][1] = hold;
+  turnZ(degrees, layer){
+    for(let i=0; i<3; i++){
+      for(let j=0; j<3; j++){
+        if(i == 1 && j == 1)
+          continue
+        rotateOnZAxis(this.cubeFrame[j][i][layer],degrees)
+      }
+    }
+    //mid
+    const axis = new THREE.Vector3(0,0,1)
+    this.cubeFrame[1][1][layer].rotateOnWorldAxis(axis,degToRad(-degrees))
   }
-  else{
-    // Rotating the corners counterclockwise
-    let hold = piece[2][layer][2];
+  
+  setZArray(direction, layer){
+  
+    if(direction > 0){
+      let hold = this.cubeFrame[0][2][layer];  
+      // Rotating the corners clockwise
+      this.cubeFrame[0][2][layer] = this.cubeFrame[0][0][layer];
+      this.cubeFrame[0][0][layer] = this.cubeFrame[2][0][layer];
+      this.cubeFrame[2][0][layer] = this.cubeFrame[2][2][layer];
+      this.cubeFrame[2][2][layer] = hold;
+  
+      // Save the initial edge this.cubeFrame
+      hold = this.cubeFrame[0][1][layer];
+  
+      // Rotating the edges clockwise
+      this.cubeFrame[0][1][layer] = this.cubeFrame[1][0][layer];
+      this.cubeFrame[1][0][layer] = this.cubeFrame[2][1][layer];
+      this.cubeFrame[2][1][layer] = this.cubeFrame[1][2][layer];
+      this.cubeFrame[1][2][layer] = hold;
+    }
+    else {
+      let hold = this.cubeFrame[0][2][layer];
+      // Rotating the corners counterclockwise
+      this.cubeFrame[0][2][layer] = this.cubeFrame[2][2][layer];
+      this.cubeFrame[2][2][layer] = this.cubeFrame[2][0][layer];
+      this.cubeFrame[2][0][layer] = this.cubeFrame[0][0][layer];
+      this.cubeFrame[0][0][layer] = hold;
+  
+      // Save the initial edge this.cubeFrame
+      hold = this.cubeFrame[0][1][layer];
+  
+      // Rotating the edges counterclockwise
+      this.cubeFrame[0][1][layer] = this.cubeFrame[1][2][layer];
+      this.cubeFrame[1][2][layer] = this.cubeFrame[2][1][layer];
+      this.cubeFrame[2][1][layer] = this.cubeFrame[1][0][layer];
+      this.cubeFrame[1][0][layer] = hold;
+    }
+    //for counter clockwise
+  }
+  setXArray(direction, layer){
+  
+    if (direction > 0) {
+      //Rotating the corners clockwise
+      let hold = this.cubeFrame[layer][0][0];  
+      this.cubeFrame[layer][0][0] = this.cubeFrame[layer][2][0];
+      this.cubeFrame[layer][2][0] = this.cubeFrame[layer][2][2];
+      this.cubeFrame[layer][2][2] = this.cubeFrame[layer][0][2];
+      this.cubeFrame[layer][0][2] = hold;
+  
+      // Save the initial edge this.cubeFrame
+      hold = this.cubeFrame[layer][0][1];
+  
+      // Rotating the edges clockwise
+      this.cubeFrame[layer][0][1] = this.cubeFrame[layer][1][0];
+      this.cubeFrame[layer][1][0] = this.cubeFrame[layer][2][1];
+      this.cubeFrame[layer][2][1] = this.cubeFrame[layer][1][2];
+      this.cubeFrame[layer][1][2] = hold;
+  
+    }
+    else {
+      // Rotating the corners counterclockwise
+      let hold = this.cubeFrame[layer][0][0];
+      this.cubeFrame[layer][0][0] = this.cubeFrame[layer][0][2];
+      this.cubeFrame[layer][0][2] = this.cubeFrame[layer][2][2];
+      this.cubeFrame[layer][2][2] = this.cubeFrame[layer][2][0];
+      this.cubeFrame[layer][2][0] = hold;
+  
+      // Save the initial edge this.cubeFrame
+      hold = this.cubeFrame[layer][0][1];
+  
+      // Rotating the edges counterclockwise
+      this.cubeFrame[layer][0][1] = this.cubeFrame[layer][1][2];
+      this.cubeFrame[layer][1][2] = this.cubeFrame[layer][2][1];
+      this.cubeFrame[layer][2][1] = this.cubeFrame[layer][1][0];
+      this.cubeFrame[layer][1][0] = hold;
+  }
+  }
+  setYArray(direction, layer){
+   
+    if (direction > 0) {
+      // Rotating the corners clockwise
+      let hold = this.cubeFrame[2][layer][2];  
+  
+      this.cubeFrame[2][layer][2] = this.cubeFrame[2][layer][0];
+      this.cubeFrame[2][layer][0] = this.cubeFrame[0][layer][0];
+      this.cubeFrame[0][layer][0] = this.cubeFrame[0][layer][2];
+      this.cubeFrame[0][layer][2] = hold;
+  
+      // Save the initial edge this.cubeFrame
+      hold = this.cubeFrame[1][layer][0];
+  
+      // Rotating the edges clockwise
+      this.cubeFrame[1][layer][0] = this.cubeFrame[0][layer][1];
+      this.cubeFrame[0][layer][1] = this.cubeFrame[1][layer][2];
+      this.cubeFrame[1][layer][2] = this.cubeFrame[2][layer][1];
+      this.cubeFrame[2][layer][1] = hold;
+    }
+    else{
+      // Rotating the corners counterclockwise
+      let hold = this.cubeFrame[2][layer][2];
+  
+      this.cubeFrame[2][layer][2] = this.cubeFrame[0][layer][2];
+      this.cubeFrame[0][layer][2] = this.cubeFrame[0][layer][0];
+      this.cubeFrame[0][layer][0] = this.cubeFrame[2][layer][0];
+      this.cubeFrame[2][layer][0] = hold;
+  
+      // Save the initial edge this.cubeFrame
+      hold = this.cubeFrame[1][layer][0];
+  
+      // Rotating the edges counterclockwise
+      this.cubeFrame[1][layer][0] = this.cubeFrame[2][layer][1];
+      this.cubeFrame[2][layer][1] = this.cubeFrame[1][layer][2];
+      this.cubeFrame[1][layer][2] = this.cubeFrame[0][layer][1];
+      this.cubeFrame[0][layer][1] = hold;
+  }
+  }
 
-    piece[2][layer][2] = piece[0][layer][2];
-    piece[0][layer][2] = piece[0][layer][0];
-    piece[0][layer][0] = piece[2][layer][0];
-    piece[2][layer][0] = hold;
-
-    // Save the initial edge piece
-    hold = piece[1][layer][0];
-
-    // Rotating the edges counterclockwise
-    piece[1][layer][0] = piece[2][layer][1];
-    piece[2][layer][1] = piece[1][layer][2];
-    piece[1][layer][2] = piece[0][layer][1];
-    piece[0][layer][1] = hold;
 }
-}
 
 
 
-let turning = 0
-let sidetoturn = ''
-let speed = 3
-let vector = speed
+
+
+  
+  
 
 
 
-function animate (){
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function isAcceptedState(){
+
+
+//   for (let i = 0, x = -5; i < 3; i++, x += 5) {
+//     for (let j = 0, y = -5; j < 3; j++, y += 5) {
+//       for (let k = 0, z = -5; k < 3; k++, z += 5) {
+//         if(cubeFrame[i][j][k] != piece[i][j][k])
+//           return false
+//       }
+//     }
+//   }
+//   return true
+// }
+
+
+
+
+
+
+function animate (cube){
   requestAnimationFrame( animate )
   
   if(turning > 0){
     switch(sidetoturn){
       case 'x0':
-        console.log('turnx ran')
-        turnX(vector, 0)
+        console.log('cube.turnx ran')
+        cube.turnX(vector, 0)
         if(turning == Math.abs(vector)){
-          setXArray(vector, 0)
+          cube.setXArray(vector, 0)
         }
         break
       case 'x1':
-        console.log('turnx ran')
-        turnX(vector, 1)
+        console.log('cube.turnx ran')
+        cube.turnX(vector, 1)
         if(turning == Math.abs(vector)){
-          setXArray(vector, 1)
+          cube.setXArray(vector, 1)
         }
         break
       case 'x2':
-        console.log('turnx ran')
-        turnX(vector, 2)
+        console.log('cube.turnx ran')
+        cube.turnX(vector, 2)
         if(turning == Math.abs(vector)){
-          setXArray(vector, 2)
+          cube.setXArray(vector, 2)
         }
         break
       case 'y0':
-        console.log('turny ran')
-        turnY(vector, 0)
+        console.log('cube.turny ran')
+        cube.turnY(vector, 0)
         if(turning == Math.abs(vector)){  
-          setYArray(vector, 0)
+          cube.setYArray(vector, 0)
         }
         break
 
       case 'y1':
-        console.log('turny ran')
-        turnY(vector, 1)
+        console.log('cube.turny ran')
+        cube.turnY(vector, 1)
         if(turning == Math.abs(vector)){  
-          setYArray(vector, 1)
+          cube.setYArray(vector, 1)
         }
         break
       
       case 'y2':
-        console.log('turny ran')
-        turnY(vector, 2)
+        console.log('cube.turny ran')
+        cube.turnY(vector, 2)
         if(turning == Math.abs(vector)){  
-          setYArray(vector, 2)
+          cube.setYArray(vector, 2)
         }
         break
       
       case 'z0':
-        console.log('turnz ran')
-        turnZ(vector, 0)
+        console.log('cube.turnz ran')
+        cube.turnZ(vector, 0)
         if(turning == Math.abs(vector)){
-          setZArray(vector, 0)
+          cube.setZArray(vector, 0)
         }
         break
 
       case 'z1':
-        console.log('turnz ran')
-        turnZ(vector, 1)
+        console.log('cube.turnz ran')
+        cube.turnZ(vector, 1)
         if(turning == Math.abs(vector)){
-          setZArray(vector, 1)
+          cube.setZArray(vector, 1)
         }
         break
 
       case 'z2':
-        console.log('turnz ran')
-        turnZ(vector, 2)
+        console.log('cube.turnz ran')
+        cube.turnZ(vector, 2)
         if(turning == Math.abs(vector)){
-          setZArray(vector, 2)
+          cube.setZArray(vector, 2)
         }
         break
     }
@@ -377,13 +423,20 @@ function animate (){
     turning -= Math.abs(vector)
   }
 
+  
   renderer.render(scene, cam)
 }
 
 
 //mao ni ang main hahaha
-init()
-animate()
+//global variables
+let turning = 0
+let sidetoturn = ''
+let speed = 3
+let vector = speed
+let rubikz = new Cube(scene)
+
+animate(rubikz)
 
 
 
